b587e023d92a746febd9df098b61301cf8e4917f
Rick Carlino <rick.carlino@gmail.com>
2016-09-17 22:05:53 -0500

    [STABLE] Start flagging logs when messages are not JSON RPC compliant

--- DEPLOYMENT.md
+++ DEPLOYMENT.md
@@ -3,7 +3,7 @@
  0. Get a nice, fresh VM with Ubuntu 16
  0. [Install Docker](https://docs.docker.com/engine/installation/linux/ubuntulinux/)
  0. `sudo docker build -t mqtt https://github.com/FarmBot/mqtt-gateway.git`
- 0. Run it:
+ 0. Run this:
 ```
   # => DANGER!!: CHANGE "staging.farmbot.io" TO YOUR API URL!!!!!!
   sudo docker run -d -e WEB_API_URL=YOUR_API_URL_HERE -p 3002:3002 -p 1883:1883 -p 80:3002 --restart=always mqtt
--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 var Server = require("mosca").Server;
 var conf = require("./config");
 var onReady = require("./on_ready");
-
+var catchBadMessage = require("./jsonrpc_sanitizer").tagMessage;
 var server = new Server({
   port: conf.mqttPort,
   http: { // for teh websockets
@@ -12,6 +12,20 @@ var server = new Server({
 });
 
 server.on("ready", onReady(server));  //on init it fires up setup()
+server.on("published", function(x) {
+  if(x && x.topic && (x.topic.slice(0,3) === "bot")) {
+    catchBadMessage(JSON.parse(x.payload.toString()), function(o) {
+      console.log(`
+
+      =============================
+      NON-JSONRPC COMPLIANT MESSAGE
+      =============================
+
+      `);
+      console.dir(o.value);
+    });
+  }
+});
 
 var authorizePublish = require("./security/authorize_publish");
 var authorizeSubscribe = require("./security/authorize_subscribe");
