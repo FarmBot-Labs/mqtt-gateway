4ae6a70801fc67c8297be6e72196216a1c2abd59
Rick Carlino <rick.carlino@gmail.com>
2016-11-11 14:04:54 -0600

    [STABLE] Lets encrypt works, need to plug it in to Mosca now

--- lets_encrypt_example.sh
+++ lets_encrypt_example.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# SSH in there and run:
+# docker exec -i -t loving_heisenberg /bin/bash
+
 letsencrypt certonly --webroot \
                       -w /app/public \
                       -d $SSL_DOMAIN \
