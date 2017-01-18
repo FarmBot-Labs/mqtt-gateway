ce94023502fc8a8b86de97a8b3162200a2c12c59
Rick Carlino <rick.carlino@gmail.com>
2017-01-18 12:27:44 -0600

    Neat logger feature

--- app/on_ready.ts
+++ app/on_ready.ts
@@ -8,7 +8,16 @@ export let onReady = (server) => () => {
     server.on("clientConnected", () => log("clientConnected"));
     server.on("clientDisconnecting", () => log("clientDisconnecting"));
     server.on("clientDisconnected", () => log("clientDisconnected"));
-    server.on("published", () => log("published"));
+    server.on("published", function () {
+        log("published");
+        try {
+            console.log(arguments[0].topic);
+            var s = String.fromCharCode.apply(null, new Uint16Array(arguments[0].payload));
+            console.log(JSON.stringify(JSON.parse(s), null, 2));
+        } catch (error) {
+            console.dir(error);
+        }
+    });
     server.on("subscribed", () => log("subscribed"));
     server.on("unsubscribed", () => log("unsubscribed"));
     server.on("error", () => log("error"));
